6bfaf0beed16e5f1be3e4a3cc4404c387f38b3c4
silent-rainmaker <wojtek.uz@gmail.com>
2023-07-09 11:33:29 +0200

    current sense calibrated

--- electricDesk/elecDesk/elecDesk.ino
+++ electricDesk/elecDesk/elecDesk.ino
@@ -1,29 +1,33 @@
 /*
 TODO
 
-current sensing + status flag diagnosis
-moving by pushbutton
-normalization
+[X] current sensing + 
+[ ] status flag diagnosis
+[ ] moving by pushbutton
+[ ] normalization
 
 errors and malfunctions detection
--missing hall
--overcurrent
--moving too long
--duty cycle
+[ ] -missing hall (only one,both)
+[ ] -overcurrent
+[ ] -moving too long
+[ ] -duty cycle (thermal)
 
-reseting
-setting upper limit
-setting lower limit
-deleteing limits
+[ ] reseting
+[ ] setting upper limit
+[ ] setting lower limit
+[ ] deleteing limits
 
-saving positions
-restoring positions
+[ ] saving positions to eeprom
+[ ] restoring positions from eeprom
 
-setting and removing detection collision sensitivity
+[ ] setting and removing detection collision sensitivity
 
-connect and control enable pins 
+[ ] connect and control enable pins 
 
 sleep mode?
+ 
+[ ] non sensitiviti for inrush current
+[ ] separate calibartion for each channel current measurements
 
 */
 #define UP 1
@@ -37,19 +41,29 @@ const int pwmPinUp =  9;
 const int pwmPinDown =  10;
 const int dirPin =  8;
 const int debugPin =  12;
-const int currentSensePin =  A3;
+const int currentSensePin =  A0;
 
 const int minPwm=64;
 const int maxPwm=192;
 const int maxPosition=5000;
 const int minPosition=0;
 const int slopeDistance=200;//hall ticks needed for smooth motor start - distance for pwn increase/decrease
+const int adcArraySize=100;//size of the array used for averaging adc readouts
 
 volatile int rawPosition = 0;         // variable for reading the pushbutton status
+volatile int adcValue = 0;
+volatile int adcCounter=0;
+volatile int adcSum=0;
+volatile int adcAvg=0;
 
 int pwmArr[slopeDistance];
+int adcArray[adcArraySize]/*={100,50,100,50,100,50,100,50,100,50}*/;//can be removed, probably
+
+
 
 void setup() {
+  zeroingAvgCurrArr();
+  
   // initialize pins as an outputs:
   pinMode(ledPin, OUTPUT);
   pinMode(pwmPinUp, OUTPUT);
@@ -65,19 +79,36 @@ void setup() {
   TCCR1A = 0b00000001; // 8bit
   TCCR1B = 0b00001010; // x8 fast pwm
   
+  
+
+  
   // Attach an interrupt to the ISR vector
-  attachInterrupt(0, hall_ISR, FALLING);
+  //attachInterrupt(0, hall_ISR, FALLING);
+  // attachInterrupt(digitalPinToInterrupt(hallPin1), hall_ISR, FALLING);
+ 
+  // Configure ADC
+  ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);  // Set ADC prescaler to 128
+  ADMUX |= (1 << REFS0);  // Set ADC reference voltage to AVCC
+  ADCSRA |= (1 << ADIE);  // Enable ADC conversion complete interrupt
+  ADCSRA |= (1 << ADEN);  // Enable ADC
+  
+  // Start first ADC conversion
+  ADCSRA |= (1 << ADSC);
+  
   Serial.begin(9600);
   Serial.println("Siema");
+  
+ 
 }
 
 void loop() {
   calculatePwm(pwmArr,slopeDistance);
+  Serial.print("position ");
   Serial.println(rawPosition);
-  delay(200);
+  delay(500);
   blinkLed();
   
-  
+  /*
   Serial.println("start moving");
   digitalWrite(debugPin, LOW);
   digitalWrite(debugPin, HIGH);
@@ -90,10 +121,35 @@ void loop() {
   delay(1000);
   goToPosition(0);
   Serial.println("min reached");
-  
-  runMotor(DOWN, 75);
-  
-  while(1) blinkLed();
+  */
+  runMotor(DOWN, 175);//down because current read will not work
+  delay(50);
+  while(1) {
+    Serial.print("ADC: ");
+    Serial.println(adcAvg);
+    //Serial.println(adcValue);
+    //delay(100);
+    //blinkLed();
+    
+    /*
+    for(int j=0;j<adcArraySize;j++){
+		
+		Serial.print(j);
+		Serial.print(": ");
+		Serial.print(adcArray[j]);
+		Serial.print(", ");
+		
+		//avgCurrent+=adcArray[j];
+		/*
+		Serial.print("subsum  ");
+		Serial.println(avgCurrent);
+		
+		}
+		*/
+	}
+	
+    
+ 
   
   
 }
@@ -105,7 +161,39 @@ void hall_ISR() {
     else {
       rawPosition--;
     }
-   Serial.println(rawPosition);
+   //Serial.println(rawPosition);
+}
+
+
+ISR(ADC_vect) {// ADC conversion complete interrupt handler
+  
+  /*adcValue = ADC; // Read ADC value
+  //Serial.print(adcValue);
+  //Serial.println(" ");
+  adcArray[adcCounter]=adcValue;
+  adcCounter++;
+  if (adcCounter==adcArraySize) {
+	  adcCounter=0; 
+	  
+  }
+  
+    ADCSRA |= (1 << ADSC);// Start the next ADC conversion
+
+    */
+    //adcValue = ADC;
+    
+    
+    adcSum+=ADC;
+    adcCounter++;
+    if (adcCounter==adcArraySize-1){
+		adcAvg=adcSum/adcArraySize;
+		adcSum=0;
+		adcCounter=0;
+	}
+	
+	
+	ADCSRA |= (1 << ADSC);// Start the next ADC conversion
+     
 }
 
 void blinkLed(){
@@ -136,7 +224,6 @@ int runMotor(int dir, int pwm){
  
   return 0;
 }
-
 
 void stopMotor(){
  runMotor(UP,0);
@@ -173,13 +260,6 @@ int goToPosition(int destPosition){
   stopMotor();
   return 0;
 }
-/*
-int* pwmCurve(int tableSize){
-  
-  int* arr = (int*)malloc(tableSize * sizeof(int));
- 
-}
-*/
 
 int momentaryPwm(int momentaryDistance, int initialDistance){
   if (initialDistance<2*slopeDistance) return minPwm;//case when the distance is to small to reach full speed and then slow down
@@ -235,4 +315,39 @@ int calculatePwm (int* arr, int arrSize){
     arr[i]= (int)tempPwm;
   }
   return 0;
+}
+
+int getCurrentAvg(){
+	//ADCSRA &= ~(1 << ADIE);//Disable adc int
+	int avgCurrent=0;
+	
+	//Serial.println("-----------------------");
+	for(int j=0;j<adcArraySize;j++){
+		/*
+		Serial.print(j);
+		Serial.print(": ");
+		Serial.println(adcArray[j]);
+		*/
+		avgCurrent+=adcArray[j];
+		/*
+		Serial.print("subsum  ");
+		Serial.println(avgCurrent);
+		*/
+	}
+	/*
+	Serial.print("sum for avg: ");
+	Serial.println(avgCurrent);
+	Serial.println("-----------------------");
+	*/
+	return avgCurrent/adcArraySize;
+	//ADCSRA |= (1 << ADIE);// Enable ADC interrupt
+	//return -255;
+}
+
+
+int zeroingAvgCurrArr(){
+	for (int i=0;i<adcArraySize;i++){
+	adcArray[i]=0;
+	}
+	
 }
